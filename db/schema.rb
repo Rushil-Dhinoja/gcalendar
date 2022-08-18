78bb743ac44c9a36ba94c5bc36dc0061e772724c
byjus-manish-s <manish66manu@gmail.com>
2022-08-18 12:03:10 +0530

    gcalendar clone

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_130454) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_055042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_130454) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
